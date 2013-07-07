246737fff00bd02a59dc7ce8c9738fcabc6b35bb
azazen09 <azazen09@gmail.com>
2013-07-07 23:54:15 +0300

    fix composer

---
+++ DependencyInjection/Compiler/ValidationPass.php
@@ -0,0 +1,52 @@
+<?php
+namespace Thrace\ComponentBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+
+use Symfony\Component\Config\Resource\FileResource;
+
+class ValidationPass implements CompilerPassInterface
+{
+    
+    protected $dir;
+    
+    public function __construct($dir)
+    {
+        $this->dir = (string) $dir;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function process(ContainerBuilder $container)
+    {  
+        if (!$container->hasParameter('validator.mapping.loader.xml_files_loader.mapping_files')) {
+            return;
+        }
+
+        $files = $container->getParameter('validator.mapping.loader.xml_files_loader.mapping_files');
+       
+        foreach ($this->getValidationFiles() as $file){
+            $files[] = $file;
+            $container->addResource(new FileResource($file));
+        }
+        
+        $container->setParameter('validator.mapping.loader.xml_files_loader.mapping_files', $files);
+    }
+    
+    protected function getValidationFiles()
+    {
+        $iterator = new \DirectoryIterator($this->dir);
+        $files = array();
+        
+        foreach ($iterator as $fileinfo){
+            if ($fileinfo->isFile()) {
+                $files[] = $fileinfo->getPathname();
+            }
+        }
+        
+        return $files;
+    }
+}
